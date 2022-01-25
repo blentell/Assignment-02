703eb559e72bae21cdd5f545d0724250511a7671
Brion Lentell <blentell@aol.com>
2022-01-25 18:42:33 -0500

    Updated answering questions and writing code

--- Questions-JS.js
+++ Questions-JS.js
@@ -29,11 +29,36 @@ const userData = [
 // Each object should have the id of the user and the amount of favorite foods they have.
 // example: [{id: '111', favoriteFoods: 2}]
 
-
+const favoriteFoods = userData.map(user => {
+  return {
+    "id: ": user.id,
+    "favoriteFoods: ": user.favorites.food.length,
+  }
+})
+console.log(favoriteFoods)
 
 // 6. Given the data above, use ".reduce" to make an array of all the names
 // of the people who have pizza as one of their favorite foods.
 // example: ['Peter', 'Mary']
 
+const pizza = userData.reduce((accumulator, user) => {
+  // console.log(user.favorites.food)
+  if (user.favorites.food.includes('pizza')) {
+    return [...accumulator, user.name]  
+  }	
+  return accumulator;
+}, []);
+console.log("We like pizza: ", pizza);
 
 // 7. Show us an example of a switch statement being used
+
+switch ("banana") {
+  case "strawberry": console.log('red and sweet');
+    break;
+  
+  case "blueberry": console.log('bluish and tangy');
+    break;
+  
+  case "banana": console.log('nasty');
+    break;
+}
